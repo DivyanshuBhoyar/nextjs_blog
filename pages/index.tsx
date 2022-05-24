85ad56fb7b6a6ac20dac37beb07b59305032d49a
DivyanshuBhoyar <divyanshubhoyar@yahoo.com>
2022-05-24 14:55:32 +0530

    css fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import Head from 'next/head' ;
-import styles from "../styles/Home.module.scss" ;
-import Link from "next/link" ;
+import Head from 'next/head';
+import styles from "../styles/Home.module.scss";
+import Link from "next/link";
 
 async function getPosts() {
   const res = await fetch(
@@ -10,43 +10,43 @@ async function getPosts() {
   return posts
 }
 
-export const getStaticProps = async ({params}) => {
-  const posts = await getPosts() ;
+export const getStaticProps = async ({ params }) => {
+  const posts = await getPosts();
   console.log(posts);
-  
+
   return {
-    props : {posts},
+    props: { posts },
     revalidate: 120
   }
 }
 
 type Post = {
-  title: string ;
-  slug: string ;
+  title: string;
+  slug: string;
 }
 
-const Home:React.FC<{posts : Post[]}> = (props) => {
-  const {posts} = props
+const Home: React.FC<{ posts: Post[] }> = (props) => {
+  const { posts } = props
   return (
     <div className={styles.container}>
       <Head>
-        <title> Hatwaar Beta's Blog</title>
+        <title> Divyanshu's Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    <h2 className = {styles.title}>Say Hello to My Blog.</h2>
+      <h2 className={styles.title}>Say Hello to My Blog.</h2>
       <main className={styles.main}>
-       <ul>
-    {
-      posts.map((post, index) => (
-        <li key = {index}>
-          <Link href ="/posts/[slug]" as ={`/posts/${post.slug}`} ><a>{post.title}</a></Link>
-        </li>
-      ))
-    }
-       </ul>
+        <ol>
+          {
+            posts.map((post, index) => (
+              <li key={index}>
+                <Link href="/posts/[slug]" as={`/posts/${post.slug}`} ><a>{post.title} </a></Link>
+              </li>
+            ))
+          }
+        </ol>
       </main>
 
-    
+
     </div>
   )
 }
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,16 +1,14 @@
 import Link from "next/link"
-import styles from "../../styles/Home.module.scss" ;
-import {useRouter} from "next/router"
+import styles from "../../styles/Home.module.scss";
+import { useRouter } from "next/router"
 import { useState } from "react";
 
-
-
-async function getPost(slug : string) {
-  const res = await fetch(
-    `${process.env.BLOG_URL}/ghost/api/v3/content/posts/slug/${slug}/?key=${process.env.CONTENT_API_KEY}&fields=title,slug,html`
-  ).then((res) => res.json())
-  const posts = res.posts
-  return posts[0]
+async function getPost(slug: string) {
+    const res = await fetch(
+        `${process.env.BLOG_URL}/ghost/api/v3/content/posts/slug/${slug}/?key=${process.env.CONTENT_API_KEY}&fields=title,slug,html`
+    ).then((res) => res.json())
+    const posts = res.posts
+    return posts[0]
 }
 //1.helloWorld - on first req --> Ghost CMS call is made
 //2.helloWorld - on further request.. --> filesystem call is made (cached)
@@ -18,53 +16,53 @@ export const getStaticPaths = () => {
     //paths --> slugs which are allowed
     //fallback --> wait ... then fire up getstatic paths again 
     return {
-        paths : [] ,
-        fallback: true 
+        paths: [],
+        fallback: true
     }
 }
 //Ghost CMS request
-export const getStaticProps = async ({params}) => {
-    const post = await getPost(params.slug) ;
+export const getStaticProps = async ({ params }) => {
+    const post = await getPost(params.slug);
     return {
-    props : {post},
-    revalidate: 120
-  }
+        props: { post },
+        revalidate: 120
+    }
 }
 type Post = {
-    title: string ,
+    title: string,
     html: string,
     slug: string
 }
-const Post: React.FC<{post: Post}> = (props) => {
-    const router = useRouter() ;
-    const {post} = props 
+const Post: React.FC<{ post: Post }> = (props) => {
+    const router = useRouter();
+    const { post } = props
 
     const [enableLoadComments, setenableLoadComments] = useState<boolean>(true)
-    if(router.isFallback) {
+    if (router.isFallback) {
         return <h2>Loading...</h2>
     }
     function loadComments() {
         setenableLoadComments(false)
-        ;(window as any).disqus_config = function () {
-            this.page.url = window.location.href ;
-            this.page.identifier = post.slug ;
-        }
+            ; (window as any).disqus_config = function () {
+                this.page.url = window.location.href;
+                this.page.identifier = post.slug;
+            }
         const script = document.createElement('script')
-        script.src  = "https://next-ghost-cms-blog.disqus.com/embed.js"
+        script.src = "https://next-ghost-cms-blog.disqus.com/embed.js"
         script.setAttribute('data-timestamp', Date.now().toString())
         document.body.appendChild(script)
     }
 
     return (
-        <div className = {styles.container}>
-            <Link href = "/">
-                <a href="">Go Back</a>
+        <div className={styles.container}>
+            <Link href="/">
+                <a href="/">Go Back</a>
             </Link>
             <h1>{post.title}</h1>
-            <div dangerouslySetInnerHTML ={{__html: post.html }} ></div>
-      {enableLoadComments && ( <p onClick = {loadComments}>Load Comments</p>)} 
-        <div id = "disqus_thread"> </div>
-        </div>
+            <div className="content" dangerouslySetInnerHTML={{ __html: post.html }} ></div>
+            {enableLoadComments && (<p onClick={loadComments}>Load Comments</p>)}
+            <div id="disqus_thread"> </div>
+        </div >
     )
 }
 export default Post
